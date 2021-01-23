541f75c2d88fc9c16c8e23b9aaf955e98fa92650
Burçin Kağan <77413560+burcinkagan@users.noreply.github.com>
2021-01-23 17:57:48 +0300

    Add project files.

---
+++ Dictionary/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> word = new MyDictionary<string>();
+            word.Add("araba");
+            word.Add("boğaz");
+            word.Add("cinsiyet");
+            word.Add("deniz");
+            word.Add("eşya");
+            word.Count();
+
+        }
+    }
+}
